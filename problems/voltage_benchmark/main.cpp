eea3e30fd30e1ccfe1e02ce0b55b4e9f0f973953
Maximilian Metti <maximilianmetti@gmail.com>
2015-09-27 22:56:23 -0700

    added domain_build function to src/domains.cpp

--- problems/voltage_benchmark/main.cpp
+++ problems/voltage_benchmark/main.cpp
@@ -21,7 +21,7 @@ extern "C"
                                          dvector *x,
                                          itsolver_param *itparam,
                                          AMG_param *amgparam,
-                                       dCSRmat *A_diag);
+                                         dCSRmat *A_diag);
 #define FASP_BSR     ON  /** use BSR format in fasp */
 }
 using namespace std;
@@ -48,61 +48,20 @@ int main()
   if ( strcmp(domain_par.mesh_file,"none")==0 ) {
     printf("\tDomain: %f x %f x %f\n",  domain_par.length_x,domain_par.length_y,domain_par.length_z);
     printf("\tGrid:   %d x %d x %d\n\n",domain_par.grid_x,domain_par.grid_y,domain_par.grid_z);
+    fflush(stdout);
   } else {
     printf("\tMesh file:      %s\n",  domain_par.mesh_file);
     printf("\tSubdomain file: %s\n",  domain_par.subdomain_file);
     printf("\tSurface file:   %s\n\n",domain_par.surface_file);
+    fflush(stdout);
   }
 
 	// build mesh
-  Mesh mesh;
-  MeshFunction<std::size_t> *subdomains;
-  MeshFunction<std::size_t> *surfaces;
-
-  // domain_build(&domain_par, &mesh, subdomains, surfaces);
-
-/*
-	// no mesh provided: use length and grid parameters
-	domain_param *dom_par; dom_par = &domain_par;
-	Mesh *meshh; meshh = &mesh;
-	MeshFunction<std::size_t> *subdoms; subdoms = subdomains;
-  MeshFunction<std::size_t> *surfs; surfs = surfaces;
-
-	if ( strcmp(dom_par->mesh_file,"none")==0 ) {
-      printf("\tDomain: %f x %f x %f\n",dom_par->length_x,dom_par->length_y,dom_par->length_z);
-      printf("\tGrid: %d x %d x %d\n",dom_par->grid_x,dom_par->grid_y,dom_par->grid_z);
-
-      BoxMesh box_mesh(-dom_par->length_x/2,-dom_par->length_y/2,-dom_par->length_z/2,dom_par->length_x/2,dom_par->length_y/2,dom_par->length_z/2, dom_par->grid_x, dom_par->grid_y, dom_par->grid_z);
-      meshh = &box_mesh;
-
-      MeshFunction<std::size_t> subdomains_object(box_mesh);
-      subdomains_object.set_all(1);
-      subdoms = &subdomains_object;
-
-      MeshFunction<std::size_t>  surfaces_object(box_mesh);
-      surfaces_object.set_all(1);
-      surfs = &surfaces_object;
-    } 
-    else { // read in mesh from specified files
-      printf(" Reading in the mesh from %s \n", dom_par->mesh_file);
-      Mesh read_mesh(dom_par->mesh_file);
-      meshh = &read_mesh;
-
-      printf(" Reading in the mesh subdomains from %s \n", dom_par->subdomain_file);
-      MeshFunction<std::size_t> subdomains_object(read_mesh, dom_par->subdomain_file);
-      subdoms = &subdomains_object;
-      
-      printf(" Reading in the mesh surfaces from %s \n", dom_par->surface_file);
-      MeshFunction<std::size_t>  surfaces_object(read_mesh, dom_par->surface_file);
-      surfs = &surfaces_object;
-    }
-
-    // mesh = meshh;
-    subdomains = subdoms;
-    surfaces = surfs;
-*/
-
-
+  dolfin::Mesh mesh;
+  dolfin::MeshFunction<std::size_t> subdomains;
+  dolfin::MeshFunction<std::size_t> surfaces;
+  dolfin::File meshOut(domain_par.mesh_output);
+  domain_build(&domain_par, &mesh, &subdomains, &surfaces, &meshOut);
 
 	// adaptivity?
 
