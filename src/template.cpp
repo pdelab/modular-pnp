7a0476b8851281dfc1c4123e465c2de59c475109
Maximilian Metti <maximilianmetti@gmail.com>
2015-09-30 22:50:01 -0700

    modified some formatting

--- src/template.cpp
+++ src/template.cpp
@@ -1,7 +1,7 @@
 /*! \file pnp.h
  *
  *  \brief Main file for PNP solver using Newton-Raphson and AMG methods.
- *		   Based on FASP and FEniCS 1.5 packages
+ *       Based on FASP and FEniCS 1.5 packages
  *
  *  \note Currently initializes the problem based on specification
  */
@@ -29,20 +29,20 @@ using namespace dolfin;
 
 int main()
 {
-	// output problem specification
-	printf("\n---------------------------------------------    "); fflush(stdout);
+  // output problem specification
+  printf("\n---------------------------------------------    "); fflush(stdout);
   printf("\n This code simulates an electrostatic system     "); fflush(stdout);
   printf("\n By solving the Poisson-Nernst-Planck system     "); fflush(stdout);
   printf("\n of a single cation and anion                    "); fflush(stdout);
   printf("\n---------------------------------------------\n\n"); fflush(stdout);
 
-	/*-------------------------------------*/
-	/*--   Setup domain and subregions   --*/
-	/*-------------------------------------*/
+  /**
+   * Setup domain and subregions
+   */
 
-	// read domain parameters
-	domain_param domain_par;
-	char domain_param_filename[] = "./problems/voltage_benchmark/domain_params.dat";
+  // read domain parameters
+  domain_param domain_par;
+  char domain_param_filename[] = "./problems/voltage_benchmark/domain_params.dat";
   domain_param_input(domain_param_filename, &domain_par);
   printf("Successfully read-in domain parameters\n");
   if ( strcmp(domain_par.mesh_file,"none")==0 ) {
@@ -56,52 +56,70 @@ int main()
     fflush(stdout);
   }
 
-	// build mesh
+  // build mesh
   dolfin::Mesh mesh;
   dolfin::MeshFunction<std::size_t> subdomains;
   dolfin::MeshFunction<std::size_t> surfaces;
   dolfin::File meshOut(domain_par.mesh_output);
   domain_build(&domain_par, &mesh, &subdomains, &surfaces, &meshOut);
 
-	// adaptivity?
-
-	// initialize boundary conditions
+  // adaptivity?
 
 
-	/*-------------------------------*/
-	/*--   Initialize expressions	 --*/
-	/*-------------------------------*/
+  /**
+   * Initialize differential equation
+   */
 
-	// dimensional analysis
+  // read coefficients and boundary values
 
-	// initialize finite elements
+  // dimensional analysis
 
-	// initialize analytic expressions
+  // initialize boundary conditions
 
+  // initialize finite elements
 
-	/*---------------------------------------*/
-	/*--   Initialize variational problem  --*/
-	/*---------------------------------------*/
-
-	// initialize bilinear, linear, and scalar forms
-
-	// interpolate analytic expressions
-
-	// assign coefficients to forms
+  // initialize analytic expressions
 
 
-	/*-----------------------*/
-	/*--   Newton solver   --*/
-	/*-----------------------*/
+  /**
+   * Initialize variational problem
+   */
 
-	// read Newton solver parameters
-	newton_param newton_par;
-	char newton_param_filename[] = "./problems/voltage_benchmark/newton_params.dat";
+  // initialize bilinear, linear, and scalar forms
+
+  // interpolate analytic expressions
+
+  // assign coefficients to forms
+
+
+  /**
+   * Newton solver
+   */
+
+  // read Newton solver parameters
+  newton_param newton_par;
+  char newton_param_filename[] = "./problems/voltage_benchmark/newton_params.dat";
   newton_param_input(newton_param_filename, &newton_par);
   printf("Successfully read-in Newton solver parameters\n");
   printf("\tNewton Maximum iterations: %d\n",newton_par.max_it);
   printf("\tNewton tolerance: %e\n",newton_par.tol);
   printf("\tNewton damping factor: %f\n",newton_par.damp_factor);
+  printf("\n");
+
+  // compute initial nonlinear residual
+
+  // build linearized system
+
+  // solve linear system
+
+  // update nonlinear solution
+
+  // check for convergence
+
+
+  printf("The code has run successfully... exiting.\n");
+  return 0;
+}tor);
   printf("\n");
 
 	// compute initial nonlinear residual
