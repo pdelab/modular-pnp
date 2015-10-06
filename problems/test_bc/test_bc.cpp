9e8b42224beec0218f490076c7881453e3febfde
Arthur Bousquet <arth.bousquet@gmail.com>
2015-10-06 10:36:05 -0400

    Boundary conditinos.c and.h are all written, need to write the test.bc

---
+++ problems/test_bc/test_bc.cpp
@@ -0,0 +1,21 @@
+/*! \file test_faspfenics.cpp
+ *
+ *  \brief Main to test FASP/FENICS interface using the Poisson problem
+ *
+ *  \note Currently initializes the problem based on specification
+ */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <dolfin.h>
+#include "boundary_conditions.h"
+
+using namespace dolfin;
+//using namespace std;
+
+
+int main()
+{
+
+    return 0;
+}
