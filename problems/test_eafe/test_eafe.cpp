3cafd5a3f7cacfa1c9dead75072c62c0f382b3e8
Maximilian Metti <maximilianmetti@gmail.com>
2015-10-13 21:28:50 -0700

    reads in constant coefficents

--- problems/test_eafe/test_eafe.cpp
+++ problems/test_eafe/test_eafe.cpp
@@ -12,24 +12,24 @@
 #include "EAFE.h"
 using namespace dolfin;
 
-// Source term (right-hand side)
-class Source : public Expression
-{
-  void eval(Array<double>& values, const Array<double>& x) const
-  {
-    double dx = x[0] - 0.5;
-    double dy = x[1] - 0.5;
-    values[0] = 10*exp(-(dx*dx + dy*dy) / 0.02);
-  }
-};
+// class Advection : public Expression
+// {
+//   void eval(Array<double>& values, const Array<double>& x) const
+//   {
+//     values[0] = 1.0e+0*x[0] + 1.0e+0*x[1] + 1.0e+0*x[2];
+//   }
+// };
 
 class Advection : public Expression
 {
-  void eval(Array<double>& values, const Array<double>& x) const
-  {
-    double x0 = x[0];
-    values[0] = 1.0e+0*x0;
-  }
+public:
+    Advection(double b_x, double b_y, double b_z): Expression(),BX(b_x),BY(b_y),BZ(b_z) {}
+    void eval(Array<double>& values, const Array<double>& x) const
+    {
+        values[0] = BX*x[0] + BY*x[1] + BZ*x[2];
+    }
+private:
+    double BX, BY, BZ;
 };
 
 // Sub domain for Dirichlet boundary condition
@@ -48,8 +48,116 @@ class DirichletBoundary : public SubDomain
 
 int main()
 {
+  // state problem
+  printf("Solving the linear PDE:\n");
+  printf("\t-div( alpha*exp(eta)*( grad(u) + <b_x,b_y,b_z>*u ) ) + gamma*exp(eta)u = 1\n");
+  printf("\n"); fflush(stdout);
 
-  int i;
+  // read in coefficients
+  char filenm[] = "./problems/test_eafe/coefficients.dat";
+  char buffer[100];
+  int val;
+  double alpha_double, eta_double, gamma_double, b_x_double, b_y_double, b_z_double;
+  FILE *fp = fopen(filenm,"r");
+  if (fp==NULL) {
+    printf("### ERROR: Could not open file %s...\n", filenm);
+  }
+  bool state = true;
+  while ( state ) {
+    double  dbuff;
+    char   *fgetsPtr;
+    
+    val = fscanf(fp,"%s",buffer);
+    if (val==EOF) break;
+    if (val!=1){ state = false; break; }
+    if (buffer[0]=='[' || buffer[0]=='%' || buffer[0]=='|') {
+        fgetsPtr = fgets(buffer,500,fp); // skip rest of line
+        continue;
+    }
+    
+    // match keyword and scan for value
+    if (strcmp(buffer,"alpha_double")==0) {
+      val = fscanf(fp,"%s",buffer);
+      if (val!=1 || strcmp(buffer,"=")!=0) {
+          state = false; break;
+      }
+      val = fscanf(fp,"%lf",&dbuff);
+      if (val!=1) { state = false; break; }
+      alpha_double = dbuff;
+      fgetsPtr = fgets(buffer,500,fp); // skip rest of line
+    }
+    
+    else if (strcmp(buffer,"eta_double")==0) {
+      val = fscanf(fp,"%s",buffer);
+      if (val!=1 || strcmp(buffer,"=")!=0) {
+          state = false; break;
+      }
+      val = fscanf(fp,"%lf",&dbuff);
+      if (val!=1) { state = false; break; }
+      eta_double = dbuff;
+      fgetsPtr = fgets(buffer,500,fp); // skip rest of line
+    }
+
+    else if (strcmp(buffer,"gamma_double")==0) {
+      val = fscanf(fp,"%s",buffer);
+      if (val!=1 || strcmp(buffer,"=")!=0) {
+          state = false; break;
+      }
+      val = fscanf(fp,"%lf",&dbuff);
+      if (val!=1) { state = false; break; }
+      gamma_double = dbuff;
+      fgetsPtr = fgets(buffer,500,fp); // skip rest of line
+    }
+
+    else if (strcmp(buffer,"b_x_double")==0) {
+      val = fscanf(fp,"%s",buffer);
+      if (val!=1 || strcmp(buffer,"=")!=0) {
+          state = false; break;
+      }
+      val = fscanf(fp,"%lf",&dbuff);
+      if (val!=1) { state = false; break; }
+      b_x_double = dbuff;
+      fgetsPtr = fgets(buffer,500,fp); // skip rest of line
+    }
+
+    else if (strcmp(buffer,"b_y_double")==0) {
+      val = fscanf(fp,"%s",buffer);
+      if (val!=1 || strcmp(buffer,"=")!=0) {
+          state = false; break;
+      }
+      val = fscanf(fp,"%lf",&dbuff);
+      if (val!=1) { state = false; break; }
+      b_y_double = dbuff;
+      fgetsPtr = fgets(buffer,500,fp); // skip rest of line
+    }
+
+    else if (strcmp(buffer,"b_z_double")==0) {
+      val = fscanf(fp,"%s",buffer);
+      if (val!=1 || strcmp(buffer,"=")!=0) {
+          state = false; break;
+      }
+      val = fscanf(fp,"%lf",&dbuff);
+      if (val!=1) { state = false; break; }
+      b_z_double = dbuff;
+      fgetsPtr = fgets(buffer,500,fp); // skip rest of line
+    }
+
+    else {
+        state = true;
+        printf(" Bad read-in: %s property unknown \n", buffer); fflush(stdout);
+    }  
+  }
+  fclose(fp);
+
+  printf("Coefficients read in are:\n");
+  printf("\talpha: \t%e\n",alpha_double);
+  printf("\teta:   \t%e\n",eta_double);
+  printf("\tgamma: \t%e\n",gamma_double);
+  printf("\tb_x:   \t%e\n",b_x_double);
+  printf("\tb_y:   \t%e\n",b_y_double);
+  printf("\tb_z:   \t%e\n",b_z_double);
+  printf("\n"); fflush(stdout);
+
 
   // Need to use Eigen for linear algebra
   parameters["linear_algebra_backend"] = "Eigen"; // or uBLAS
@@ -70,14 +178,14 @@ int main()
 
   // Define analytic expressions
   printf("Define analytic expressions\n"); fflush(stdout);
-  dolfin::Constant alpha(1.0e-0);
+  dolfin::Constant alpha(alpha_double);
   
-  Advection betaExpression;
+  Advection betaExpression(b_x_double,b_y_double,b_z_double);
   dolfin::Function beta(CG);
   beta.interpolate(betaExpression);
 
-  dolfin::Constant gamma(1.0);
-  dolfin::Constant eta(0.0);
+  dolfin::Constant gamma(gamma_double);
+  dolfin::Constant eta(eta_double);
   dolfin::Constant f(1.0);
 
 
