d5aeb7a8e240e145ae30aa1b98bd084e0263651c
Arthur Bousquet <arth.bousquet@gmail.com>
2016-02-02 23:20:41 -0500

    added check electric

--- include/newton_functs.h
+++ include/newton_functs.h
@@ -66,10 +66,14 @@ void domain_build (domain_param *domain_par,
            dolfin::MeshFunction<size_t> *surfaces);
 
 unsigned int check_local_entropy (dolfin::Function *cation,
-                                  double cation_valency,
-                                  dolfin::Function *anion,
-                                  double anion_valency,
-                                  dolfin::Function *voltage,
+                                 double cation_valency,
+                                 dolfin::Function *anion,
+                                 double anion_valency,
+                                 dolfin::Function *voltage,
+                                 dolfin::Mesh *target_mesh,
+                                 double entropy_tol);
+
+unsigned int check_electric_field (dolfin::Function *voltage,
                                   dolfin::Mesh *target_mesh,
                                   double entropy_tol);
 
