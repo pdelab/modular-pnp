35769be6499ad64c48d5efed8f2a7a509b130529
Arthur Bousquet <arth.bousquet@gmail.com>
2017-08-29 11:08:19 -0400

    added the scripts

--- benchmarks/physic_bench/OutFlow.py
+++ benchmarks/physic_bench/OutFlow.py
@@ -35,44 +35,51 @@ ds = ds(subdomain_data = boundaries)
 n = FacetNormal(mesh)
 
 u1 = Function(V,"DATA_0/velocity_solution.xml")
-u2 = Function(V,"DATA_2/velocity_solution.xml")
-u3 = Function(V,"DATA_4/velocity_solution.xml")
-u4 = Function(V,"DATA_6/velocity_solution.xml")
-u5 = Function(V,"DATA_8/velocity_solution.xml")
+u2 = Function(V,"DATA_50/velocity_solution.xml")
+u3 = Function(V,"DATA_100/velocity_solution.xml")
+u4 = Function(V,"DATA_150/velocity_solution.xml")
+u5 = Function(V,"DATA_200/velocity_solution.xml")
+u6 = Function(V,"DATA_250/velocity_solution.xml")
 
+E1 = 3.0/4.0*inner(u1,n)*ds(1)
+E2 = 3.0/4.0*inner(u2,n)*ds(1)
+E3 = 3.0/4.0*inner(u3,n)*ds(1)
+E4 = 3.0/4.0*inner(u4,n)*ds(1)
+E5 = 3.0/4.0*inner(u5,n)*ds(1)
+E6 = 3.0/4.0*inner(u6,n)*ds(1)
 
-E1 = inner(u1,n)*ds(1)
-E2 = inner(u2,n)*ds(1)
-E3 = inner(u3,n)*ds(1)
-E4 = inner(u4,n)*ds(1)
-E5 = inner(u5,n)*ds(1)
+v1 = assemble(E1)*1E3
+v2 = assemble(E2)*1E3
+v3 = assemble(E3)*1E3
+v4 = assemble(E4)*1E3
+v5 = assemble(E5)*1E3
+v6 = assemble(E6)*1E3
 
-v1 = assemble(E1)
-v2 = assemble(E2)
-v3 = assemble(E3)
-v4 = assemble(E4)
-v5 = assemble(E5)
+y = np.array([v1,v2,v3,v4,v5,v6])
+x = np.array([0,50,100,150,200,250])
 
-y = np.array([v1,v2,v3,v4,v5])
-x = np.array([0,2,4,6,8])
+print y[1:]
+print x[1:]
+print y[1:]/x[1:]
 
 plt.figure(1)
 plt.clf()
 plt.plot(x, y,"-*")
 # plt.plot(x, x*0.065/10.0,"--")
 # plt.show()
-plt.ylabel("Average outgoing flow")
-plt.xlabel("Voltage drop $\delta V$")
+plt.ylabel("Average outgoing flow in mm/s")
+plt.xlabel("Voltage drop $\delta V$ in mV")
 plt.savefig("OutFlow.eps")
 plt.savefig("OutFlow.png")
 
 
 
 N0=np.log(np.array([0.0192247,2.35473e-07,1.19454e-13])) # V=0
-N2=np.log(np.array([0.0192071,2.40205e-07,3.43298e-13])) # V=2
-N4=np.log(np.array([0.0191623,2.39543e-07,3.38475e-13])) # V=4
-N6=np.log(np.array([0.0191008,2.37633e-07,1.88464e-12])) # V=6
-N8=np.log(np.array([0.018905,2.48055e-07,1.84684e-12])) # V=8
+N2=np.log(np.array([0.0192246,2.31708e-07,1.92468e-13])) # V=50
+N4=np.log(np.array([0.0191769,2.40639e-07,5.07067e-13,])) # V=100
+N6=np.log(np.array([0.0191326,2.41349e-07,6.93888e-13])) # V=150
+N8=np.log(np.array([0.0190566,2.48112e-07,5.19014e-13])) # V=200
+N9=np.log(np.array([0.0189578,2.56926e-07,8.25984e-13]))  # V=250
 xn =np.array([1,2,3])
 
 
@@ -83,9 +90,10 @@ plt.plot(xn, N2,"-*")
 plt.plot(xn, N4,"-*")
 plt.plot(xn, N6,"-*")
 plt.plot(xn, N8,"-*")
+plt.plot(xn, N9,"-*")
 # plt.plot(x, x*0.065/10.0,"--")
 # plt.show()
-plt.legend(("$\delta V=0$","$\delta V=2$","$\delta V=4$","$\delta V=6$","$\delta V=8$"))
+plt.legend(("$\delta V=0$ mV","$\delta V=50$ mV","$\delta V=100$ mV","$\delta V=150$ mV","$\delta V=200$ mV","$\delta V=250$ mV"))
 plt.xlabel("Newton Iteration")
 plt.ylabel("log of the Relative Residual")
 plt.savefig("Residual.eps")
--- benchmarks/physic_bench/dimensional_analysis.py
+++ benchmarks/physic_bench/dimensional_analysis.py
@@ -20,3 +20,43 @@ print -12-23+2*19-25
 print 80*8.854187817*1.38064853*300/(1.6021766209**2*6.02214085774*10**2)
 
 print Epsilon_ref
+
+Fo = "./output_PNP/"
+
+mesh = Mesh(Fo+"accepted_mesh.xml.gz")
+P1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+# TH = P1 * P1 * P1
+TH = MixedElement([P1, P1, P1])
+V = FunctionSpace(mesh,TH)
+pnp = Function(V,Fo+"accepted_solution.xml")
+
+print "Number fo subspace", V.num_sub_spaces()
+
+dofs_phi = V.sub(0).dofmap().dofs()
+dofs_eta1 = V.sub(1).dofmap().dofs()
+dofs_eta2 = V.sub(2).dofmap().dofs()
+
+ar = pnp.vector().array()
+for i in dofs_phi:
+    ar[i]=ar[i]*1000/34.81356616
+for i in dofs_eta1:
+    ar[i]=np.exp(ar[i])*100.0
+for i in dofs_eta2:
+    ar[i]=np.exp(ar[i])*100.0
+
+pnp.vector()[:] = ar
+
+F1 = File(Fo+"accepted_solution2.pvd")
+F1 << pnp
+
+
+## NS
+# Fo = "./DATA_250/"
+# mesh = Mesh("mesh3.xml.gz")
+# V2 = FunctionSpace(mesh, "RT", 1)
+# u = Function(V2,Fo+"velocity_solution.xml")
+# ar = u.vector().array()
+# ar=ar*3.0*1E3
+# u.vector()[:] = ar
+# F1 = File(Fo+"velocity.pvd")
+# F1 << u
